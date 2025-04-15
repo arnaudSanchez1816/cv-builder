45e3c6bbc2ca27f767a9ea1e7e4f8cbc99c09da3
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2025-04-15 16:50:11 +0200

    Add form and form item components

---
+++ src/components/EditForm.jsx
@@ -0,0 +1,39 @@
+import { Icon } from "@iconify/react/dist/iconify.js"
+import "../styles/EditForm.css"
+
+function EditForm({ children, onSubmit, onCancel = null, onDelete = null }) {
+    return (
+        <form className="edit-form padding-medium" onSubmit={onSubmit}>
+            {children}
+            <div className="edit-form-controls">
+                {onDelete && (
+                    <button
+                        type="button"
+                        onClick={onDelete}
+                        className="edit-form-button mr-[auto]"
+                    >
+                        Delete
+                    </button>
+                )}
+                {onCancel && (
+                    <button
+                        type="button"
+                        onClick={onCancel}
+                        className="edit-form-button"
+                    >
+                        Cancel
+                    </button>
+                )}
+                {onSubmit && (
+                    <button type="submit" className="edit-form-submit">
+                        <Icon icon="mdi:check" />
+                        <span>|</span>
+                        <span>Save</span>
+                    </button>
+                )}
+            </div>
+        </form>
+    )
+}
+
+export default EditForm
--- src/components/PersonalDetailsEditSection.jsx
+++ src/components/PersonalDetailsEditSection.jsx
@@ -1,77 +1,35 @@
 import "../styles/PersonalDetailsEditSection.css"
+import EditForm from "./EditForm"
+import EditFormInput from "./EditFormItem"
 import EditSection from "./EditSection"
 
 function PersonalDetailsEditForm() {
+    const onSubmit = (e) => {}
+    const onCancel = (e) => {}
+
     return (
-        <form className="edit-personal-details-form padding-medium">
-            <div className="edit-form-item">
-                <label htmlFor="full-name">Full name</label>
-                <input
-                    type="text"
-                    className="rounded-medium"
-                    name="full-name"
-                    id="full-name"
-                />
-            </div>
-            <div className="edit-form-item">
-                <label htmlFor="job-title">
-                    Job title{" "}
-                    <span className="text-grey-400 text-[0.8em]">optional</span>
-                </label>
-                <input
-                    type="text"
-                    className="rounded-medium"
-                    name="job-title"
-                    id="job-title"
-                />
-            </div>
-            <div className="edit-form-item">
-                <label htmlFor="email">
-                    Email{" "}
-                    <span className="text-grey-400 text-[0.8em]">
-                        recommended
-                    </span>
-                </label>
-                <input
-                    type="email"
-                    className="rounded-medium"
-                    name="email"
-                    id="email"
-                />
-            </div>
-            <div className="edit-form-item">
-                <label htmlFor="phone">
-                    Phone number{" "}
-                    <span className="text-grey-400 text-[0.8em]">
-                        recommended
-                    </span>
-                </label>
-                <input
-                    type="tel"
-                    className="rounded-medium"
-                    name="phone"
-                    id="phone"
-                />
-            </div>
-            <div className="edit-form-item">
-                <label htmlFor="address">
-                    Address{" "}
-                    <span className="text-grey-400 text-[0.8em]">
-                        recommended
-                    </span>
-                </label>
-                <input
-                    type="text"
-                    className="rounded-medium"
-                    name="address"
-                    id="address"
-                />
-            </div>
-            <div className="edit-form-controls">
-                <button type="button">Cancel</button>
-                <button type="submit">Save</button>
-            </div>
-        </form>
+        <EditForm onSubmit={onSubmit} onCancel={onCancel}>
+            <EditFormInput id="full-name" value="Full name" label="Full name" />
+            <EditFormInput id="job-title" value="Job title" label="Job title" />
+            <EditFormInput
+                id="email"
+                value="Email"
+                label="Email"
+                options={{ recommended: true }}
+            />
+            <EditFormInput
+                id="phone"
+                value="0605075425"
+                label="Phone number"
+                options={{ recommended: true }}
+            />
+            <EditFormInput
+                id="address"
+                value="Address"
+                label="Address"
+                options={{ recommended: true }}
+            />
+        </EditForm>
     )
 }
 
